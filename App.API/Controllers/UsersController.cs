afa787fd61fc0ca3a7c6c63c3fb2982b0e561194
Ahmed Gamal <gemi6276@gmail.com>
2024-05-04 09:09:33 +0200

    setup angular

--- App.API/Controllers/UsersController.cs
+++ App.API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using App.API.DataContext;
+using App.API.Entites;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.API.Controllers
@@ -7,8 +9,37 @@ namespace App.API.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly DataDbContext _context;
 
-      
+        public UsersController( DataDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IReadOnlyList<AppUser>> GetAllUsers()
+        {
+
+            var users = _context.Users.ToList();
+
+            if (users is null)
+                return NotFound();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUserById( int id)
+        {
+
+            var user = _context.Users.Find(id);
+
+            if (user is null)
+                return NotFound();
+
+            return Ok(user);
+        }
 
 
 
